e19b219bba5fbe3fd8daf694f3e31ee3001c85eb
alex <alexandrk_@hotmail.com>
2019-11-18 13:24:04 -0500

    BACKLOG-11283 Update pom, refactor route group

--- src/javascript/Dashboard/Dashboard.jsx
+++ src/javascript/Dashboard/Dashboard.jsx
@@ -1,28 +1,26 @@
 import React from 'react';
 import {registry} from '@jahia/registry';
 import {useHistory} from 'react-router-dom';
-import {
-    PrimaryNavItemsGroup,
-    PrimaryNavItem
-} from '@jahia/moonstone';
+import {PrimaryNavItem} from '@jahia/moonstone';
 import Bars from '@jahia/moonstone/dist/icons/Bar';
 import {registerRoute} from './Dashboard.route';
+
+const ROUTE = '/dashboard';
 
 const DashboardGroup = () => {
     const history = useHistory();
     return (
-        <PrimaryNavItemsGroup>
-            <PrimaryNavItem isSelected={history.location.pathname.endsWith('/dashboard')}
-                            label="Dashboard"
-                            icon={<Bars/>}
-                            onClick={() => history.push('/dashboard')}/>
-        </PrimaryNavItemsGroup>
+        <PrimaryNavItem key={ROUTE}
+                        isSelected={history.location.pathname.endsWith(ROUTE)}
+                        label="Dashboard"
+                        icon={<Bars/>}
+                        onClick={() => history.push(ROUTE)}/>
     );
 };
 
 export const registerDashboard = () => {
     registerRoute();
-    registry.add('dashboardGroup', {
+    registry.add('dashboardGroupItem', {
         type: 'topNavGroup',
         target: ['nav-root-top:1'],
         render: () => <DashboardGroup/>
