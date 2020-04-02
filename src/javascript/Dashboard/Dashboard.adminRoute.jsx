3ceb82fd772c053ff3faff1cc303ae8b3f726941
Thomas Draier <tdraier@jahia.com>
2020-04-02 11:27:18 +0200

    BACKLOG-13024 : Fixed route to dashboardframe (#16)

--- src/javascript/Dashboard/Dashboard.adminRoute.jsx
+++ src/javascript/Dashboard/Dashboard.adminRoute.jsx
@@ -20,28 +20,28 @@ export const registerAdminRoute = () => {
         icon: <File/>,
         label: 'jahia-dashboard:jahia-dashboard.workspace.files',
         isSelectable: true,
-        iframeUrl: `${window.contextJsParameters.contextPath}/cms/adminframe/default/$ui-lang/${window.contextJsParameters.user.path}.files.html`
+        iframeUrl: `${window.contextJsParameters.contextPath}/cms/dashboardframe/default/$ui-lang/${window.contextJsParameters.user.path}.files.html`
     });
     registry.add('adminRoute', 'projects', {
         targets: ['dashboard:30'],
         icon: <SiteWeb/>,
         label: 'jahia-dashboard:jahia-dashboard.workspace.projects',
         isSelectable: true,
-        iframeUrl: `${window.contextJsParameters.contextPath}/cms/adminframe/default/$ui-lang/${window.contextJsParameters.user.path}.projects.html`
+        iframeUrl: `${window.contextJsParameters.contextPath}/cms/dashboardframe/default/$ui-lang/${window.contextJsParameters.user.path}.projects.html`
     });
     registry.add('adminRoute', 'pages', {
         targets: ['dashboard:40'],
         icon: <FileContent/>,
         label: 'jahia-dashboard:jahia-dashboard.workspace.pages',
         isSelectable: true,
-        iframeUrl: `${window.contextJsParameters.contextPath}/cms/adminframe/default/$ui-lang/${window.contextJsParameters.user.path}.pages.html`
+        iframeUrl: `${window.contextJsParameters.contextPath}/cms/dashboardframe/default/$ui-lang/${window.contextJsParameters.user.path}.pages.html`
     });
     registry.add('adminRoute', 'tasks', {
         targets: ['dashboard:50'],
         icon: <Task/>,
         label: 'jahia-dashboard:jahia-dashboard.workspace.tasks',
         isSelectable: true,
-        iframeUrl: `${window.contextJsParameters.contextPath}/cms/adminframe/default/$ui-lang/${window.contextJsParameters.user.path}.tasks.html`
+        iframeUrl: `${window.contextJsParameters.contextPath}/cms/dashboardframe/default/$ui-lang/${window.contextJsParameters.user.path}.tasks.html`
     });
 };
 
